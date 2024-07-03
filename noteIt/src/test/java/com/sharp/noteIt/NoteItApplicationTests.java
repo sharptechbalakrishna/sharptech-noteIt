e4b15e9b06bc90eba10d9b6c50ecdbc1c57087c7
Admin <sharptechbalakrishna@gmail.com>
2024-07-03 17:40:55 +0530

    added project

---
+++ noteIt/src/test/java/com/sharp/noteIt/NoteItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharp.noteIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
