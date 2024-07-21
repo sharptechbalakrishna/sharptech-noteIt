a3cc6a2a01efd88e277a1c83b9d400a3e59e62ec
Admin <sharptechbalakrishna@gmail.com>
2024-07-21 23:21:42 +0530

    Code cleanup
    Signed-off-by: Admin <sharptechbalakrishna@gmail.com>

--- noteIt/src/main/java/com/sharp/noteIt/controller/UserController.java
+++ noteIt/src/main/java/com/sharp/noteIt/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.sharp.noteIt.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,22 +15,22 @@ import com.sharp.noteIt.service.CustomerServiceI;
 
 @RestController
 public class UserController {
-	
+
 	@Autowired
 	CustomerServiceI service;
-	
+
 	@PostMapping("/register")
-	public String saveCustomer(@RequestBody CustomerRequest request) {
-		
-		service.saveCustomer(request);
-		return "Success";
-		
+	public ResponseEntity<?> saveCustomer(@RequestBody CustomerRequest request) {
+
+		CustomerDoc saveCustomer = service.saveCustomer(request);
+		return new ResponseEntity<>(saveCustomer, HttpStatus.CREATED);
+
 	}
-	
-	 @GetMapping("/login")
-	    public CustomerDoc login(@RequestParam String phone, @RequestParam String password) {
-	        System.out.println("Attempting to login with phone: " + phone); // Print to console
-	        return service.login(phone, password);
-	    }
-	
+
+	@GetMapping("/login")
+	public CustomerDoc login(@RequestParam String phone, @RequestParam String password) {
+		System.out.println("Attempting to login with phone: " + phone); // Print to console
+		return service.login(phone, password);
+	}
+
 }
